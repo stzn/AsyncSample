d0a2ec685ebe207a6f6ba264f88f8a4820da1832
stzn <sztk1209@gmail.com>
2021-05-03 11:36:02 +0900

    add more view models (using DispatchGroup and async let)

--- AsyncSample/User/UserViewModel.swift
+++ AsyncSample/User/UserViewModel.swift
@@ -14,6 +14,8 @@ final class SyncUserViewModel: ObservableObject {
     var isPremium: Bool { user?.isPremium ?? false }
 
     func getUserInfo() {
+        isLoading = true
+        user = nil
         let id = getUserId()
         let name = getUserName(id: id)
         let age = getUserAge(id: id)
@@ -22,6 +24,7 @@ final class SyncUserViewModel: ObservableObject {
                          name: name,
                          age: age,
                          isPremium: isPremium)
+        isLoading = false
     }
 
     func getUserId() -> UserID {
@@ -46,13 +49,15 @@ final class SyncUserViewModel: ObservableObject {
 
 }
 
-final class CompletionUserViewModel: ObservableObject {
+final class DispatchUserViewModel: ObservableObject {
     @Published var user: User?
     @Published var isLoading: Bool = false
 
     var isPremium: Bool { user?.isPremium ?? false }
 
     func getUserInfo() {
+        isLoading = true
+        user = nil
         getUserId { [weak self] id in
             self?.getUserName(id: id) { [weak self] name in
                 self?.getUserAge(id: id) { [weak self] age in
@@ -62,6 +67,7 @@ final class CompletionUserViewModel: ObservableObject {
                                               name: name,
                                               age: age,
                                               isPremium: isPremium)
+                            self?.isLoading = false
                         }
                     }
                 }
@@ -102,6 +108,7 @@ final class AsyncUserViewModel: ObservableObject {
 
     func getUserInfo() {
         isLoading = true
+        user = nil
         async { @MainActor in
             let id = await getUserId()
             let name = await getUserName(id: id)
@@ -166,3 +173,141 @@ final class AsyncUserViewModel: ObservableObject {
         }
     }
 }
+
+final class DispatchGroupUserViewModel: ObservableObject {
+    @Published var user: User?
+    @Published var isLoading: Bool = false
+
+    var isPremium: Bool { user?.isPremium ?? false }
+
+    func getUserInfo() {
+        isLoading = true
+        user = nil
+
+        let group = DispatchGroup()
+        getUserId { [weak self] id in
+            var name = ""
+            var age = 0
+            var isPremium = false
+
+            self?.getUserName(id: id, group: group) { name = $0 }
+            self?.getUserAge(id: id, group: group) { age = $0 }
+            self?.getUserIsPremium(id: id, group: group) { isPremium = $0 }
+            group.notify(queue: .main) { [weak self] in
+                self?.user = User(id: id,
+                                  name: name,
+                                  age: age,
+                                  isPremium: isPremium)
+                self?.isLoading = false
+            }
+        }
+    }
+
+    func getUserId(completion: @escaping (UserID) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) {
+            completion(UUID())
+        }
+    }
+
+    func getUserName(id: UserID, group: DispatchGroup, completion: @escaping (String) -> Void) {
+        group.enter()
+        DispatchQueue.global().async(group: group) {
+            sleep(2)
+            group.leave()
+            completion("User Name")
+        }
+    }
+
+    func getUserAge(id: UserID, group: DispatchGroup, completion: @escaping (Int) -> Void) {
+        group.enter()
+        DispatchQueue.global().async(group: group) {
+            sleep(2)
+            group.leave()
+            completion(20)
+        }
+    }
+
+    func getUserIsPremium(id: UserID, group: DispatchGroup, completion: @escaping (Bool) -> Void) {
+        group.enter()
+        DispatchQueue.global().async(group: group) {
+            sleep(2)
+            group.leave()
+            completion(true)
+        }
+    }
+}
+
+final class ConcurrentAsyncUserViewModel: ObservableObject {
+    @Published var user: User?
+    @Published var isLoading: Bool = false
+
+    var isPremium: Bool { user?.isPremium ?? false }
+
+    func getUserInfo() {
+        isLoading = true
+        user = nil
+        async { @MainActor in
+            let id = await self.getUserId()
+            async let name = getUserName(id: id)
+            async let age = getUserAge(id: id)
+            async let isPremium = getUserIsPremium(id: id)
+            user = await User(id: id, name: name, age: age, isPremium: isPremium)
+            isLoading = false
+        }
+    }
+
+    func getUserId() async -> UserID {
+        await withUnsafeContinuation { continuation in
+            getUserId { continuation.resume(returning: $0) }
+        }
+    }
+
+    func getUserName(id: UserID) async -> String {
+        await withUnsafeContinuation { continuation in
+            getUserName(id: id) { continuation.resume(returning: $0) }
+        }
+    }
+
+    func getUserAge(id: UserID) async -> Int {
+        await withUnsafeContinuation { continuation in
+            getUserAge(id: id) { continuation.resume(returning: $0) }
+        }
+    }
+
+    func getUserIsPremium(id: UserID) async -> Bool {
+        await withUnsafeContinuation { continuation in
+            getUserIsPremium(id: id) { continuation.resume(returning: $0) }
+        }
+    }
+
+    func getUserId(completion: @escaping (UserID) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completion(UUID())
+        }
+    }
+
+    func getUserName(id: UserID, completion: @escaping (String) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completion("User Name")
+        }
+    }
+
+    func getUserAge(id: UserID, completion: @escaping (Int) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completion(20)
+        }
+    }
+
+    func getUserIsPremium(id: UserID, completion: @escaping (Bool) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completion(true)
+        }
+    }
+
+    func getIsPremium(id: UserID, completion: @escaping (Bool) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completion(true)
+        }
+    }
+}
+
